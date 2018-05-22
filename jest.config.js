087ef49bd4ad2e2a68f4996fee8cb3b584e41ea9
Vanessa Ating <atingenkay@gmail.com>
2018-05-22 12:13:18 +0100

    chore(configure-unit-testing): configure unit testing and code coverage
    - install dependencies for unit testing
    - set up jest test runner config with the required details
    - set up enzyme for unit testing of the components
    
    [Starts #157507759]

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(roots/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "snapshotSerializers": ["enzyme-to-json/serializer"],
+  "setupTestFrameworkScriptFile": "roots/setupEnzyme.ts",
+}
